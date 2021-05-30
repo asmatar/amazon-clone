311ce192c301b6fa5a16c30a0540de7799f66a03
asmatar <deruelle.arthur@gmail.com>
2021-05-30 15:43:24 +0200

    home page static

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import Product from './Product'
 
 function Home() {
     return (
@@ -8,7 +9,8 @@ function Home() {
 
             </Banner>
             <Content>
-                I'm the content
+                <Product />
+                <Product />
             </Content>
         </Container>
     )
@@ -29,11 +31,13 @@ const Banner = styled.div`
     // make sure that no matter that big is the windows screen, it'll cover it
     background-size: cover;
     // to apply a gradient to the background, the opacity go from 1 to 0 from top to bottom
-    mask-image: linear-gradient(to bottom, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0) )
+    mask-image: linear-gradient(to bottom, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0));
+    z-index: 1;
 `
 const Content = styled.div`
-    background: white;
+    // background: white;
     padding-left: 10px;
     padding-right: 10px;
     margin-top: -350px;
+    display: flex;
 `
---
+++ src/components/Product.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Product() {
+    return (
+        <Container>
+            <Title>Iphone</Title>
+            <Price> 1449 $</Price>
+            <Rating> ****</Rating>
+            <Image src='https://images-na.ssl-images-amazon' ></Image>
+            <ActionSection>
+            <AddToCartButton> add to cart</AddToCartButton> 
+
+            </ActionSection>
+        </Container>
+    )
+}
+
+export default Product
+
+const Container = styled.div`
+    background-color: white;
+    z-index: 100;
+    max-height: 400px;
+    padding: 20px;
+    margin: 10px;
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+`
+const Title = styled.span``
+const Price = styled.span`
+    font-weight: 500;
+    margin-top: 3px;
+`
+const Rating = styled.div``
+const Image = styled.img`
+    max-height: 200px;
+    object-fit: contain;
+`
+const AddToCartButton = styled.button`
+    background-color: #f0c14b;
+    width: 100px;
+    border: 2px solid #a88734;
+    border-radius: 2px;
+    height: 30px;
+`
+const ActionSection = styled.div`
+    margin-top: 12px;
+    display: grid;
+    place-items: center;
+`
