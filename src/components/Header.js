02a8985ce6dbc06a49210cfdc13996c82c19a8dd
asmatar <deruelle.arthur@gmail.com>
2021-05-30 14:17:09 +0200

    header static CSS

---
+++ src/components/Header.js
@@ -0,0 +1,119 @@
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import React from 'react';
+import styled from 'styled-components';
+function Header() {
+    return (
+        <Container>
+            <HeaderLogo>
+                <img src={"https://i.imgur.com/7I9Was5.png"} alt="image logo" />
+            </HeaderLogo>
+
+            <HeaderOptionAdress>
+                <LocationOnIcon />
+                <HeaderOption>
+                    <OptionLineOne>Hello</OptionLineOne>
+                    <OptionLineTwo>Select your adress</OptionLineTwo>
+                </HeaderOption>
+            </HeaderOptionAdress>
+
+            <HeaderSearch>
+                <HeaderSearchInput type='text' />
+                <HeaderSearchIconContainer>
+                    <SearchIcon />
+                </HeaderSearchIconContainer>
+            </HeaderSearch>
+
+            <HeaderNavItems>
+                <HeaderOption>
+                    <OptionLineOne>Hello, nazary</OptionLineOne>
+                    <OptionLineTwo> Account & List</OptionLineTwo>
+                </HeaderOption>
+
+                <HeaderOption>
+                    <OptionLineOne>Return</OptionLineOne>
+                    <OptionLineTwo>& Order</OptionLineTwo>
+                </HeaderOption>
+
+                <HeaderOptionCart>
+                    <ShoppingBasketIcon />
+                    <CartCount>4</CartCount>
+                </HeaderOptionCart>
+            </HeaderNavItems>
+        </Container>
+      
+    )
+}
+
+export default Header
+
+const Container = styled.div`
+    height: 60px;
+    background-color: #0F1111;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    color: white;
+`
+const HeaderLogo = styled.div`
+
+    img {
+        width: 100px;
+        margin-left: 11px;
+    }
+`
+const HeaderOptionAdress = styled.div`
+    padding-left: 9px;
+    display: flex;
+    align-items: center;
+`
+const OptionLineOne = styled.div``
+const OptionLineTwo = styled.div`
+font-weight: 700;
+`
+const HeaderSearch = styled.div`
+display: flex;
+flex-grow: 1;
+height: 40px;
+border-radius: 4px;
+overflox: hidden;
+margin-left: 4px;
+background-color: white;
+:focus-within {
+    box-shadow: 0 0 0 3px #F90;
+}
+`
+const HeaderSearchInput = styled.input`
+flex-grow:1;
+border: 0; 
+
+:focus {
+    outline: none;
+}
+`
+const HeaderSearchIconContainer = styled.div`
+background-color: #febd69;
+width: 45px;
+color: black;
+display: flex;
+justify-content: center;
+align-items: center;
+
+`
+const HeaderNavItems= styled.div`
+display: flex;
+
+`
+const HeaderOption= styled.div`
+padding: 10px 9px 10px 9px;
+
+`
+const HeaderOptionCart= styled.div`
+display: flex;
+align-items: center;
+padding-right: 8px;
+`
+const CartCount= styled.div`
+    padding-left: 4px;
+`
