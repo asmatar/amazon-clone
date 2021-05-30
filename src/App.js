5d4ac10e3af5accc6f863e37cec3519e08f3dfdb
asmatar <deruelle.arthur@gmail.com>
2021-05-30 23:17:40 +0200

    set up firebase and get data from cart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Cart from './components/Cart';
 import Header from './components/Header';
 import Home from './components/Home';
+import { db } from './firebase';
 
 function App() {
+
+  const [cartItems, setCartItems] = useState([])
+
+  const getCartItems = () => {
+    db.collection('cartItems').onSnapshot((snapshot)=> {
+      console.log(snapshot)
+      let tempCartItems = []
+
+      tempCartItems = snapshot.docs.map((doc)=> (
+        {
+          id: doc.id,
+          product: doc.data()
+        }
+      ));
+      setCartItems(tempCartItems)
+    })
+  }
+
+  useEffect(() => {
+    getCartItems();
+  }, [])
+
+  console.log(cartItems)
+
   return (
     <Router>
       <div className="App">
